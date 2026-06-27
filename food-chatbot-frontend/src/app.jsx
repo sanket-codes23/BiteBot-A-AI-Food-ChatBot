83f8b4cf8aa6c1c424a1db390683a7e762422ced
Sanket Singhal <sanketsinghal23@gmail.com>
2026-06-27 17:02:57 +0530

    Fix Linux import case

--- food-chatbot-frontend/src/app.jsx
+++ food-chatbot-frontend/src/app.jsx
@@ -1,11 +1,11 @@
-import "./App.css";
+import "./app.css";
 
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import PopularDishes from "./components/PopularDishes";
-import Features from "./components/Features";
-import Chatbot from "./components/Chatbot";
-import Footer from "./components/Footer";
+import Navbar from "./components/navbar";
+import Hero from "./components/hero";
+import PopularDishes from "./components/populardishes";
+import Features from "./components/features";
+import Chatbot from "./components/chatbot";
+import Footer from "./components/footer";
 
 function App() {
 
@@ -13,19 +13,19 @@ function App() {
 
         <>
 
-            <Navbar/>
+            <navbar/>
 
-            <Hero/>
+            <hero/>
 
-            <PopularDishes/>
+            <populardishes/>
 
-            <Features/>
+            <features/>
 
             
 
-            <Footer/>
+            <footer/>
 
-            <Chatbot/>
+            <chatbot/>
 
         </>
 
